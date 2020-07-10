ed13df07934755d70ca45875394712ae3ce7d468
AlfaPigeon <oguzkaan.ozsoy@hotmail.com>
2020-07-10 21:36:00 +0300

    WhiteList added for safety

--- Backend/372_Server.py
+++ Backend/372_Server.py
@@ -1,8 +1,17 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+WhiteList = ['/', '/index.html']
+
+
 class requestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+
+        if(self.path not in WhiteList):
+            self.send_response(403)
+            self.end_headers()
+            return
+
         if self.path == '/':
             self.path = '/index.html'
         try:
@@ -11,18 +20,19 @@ class requestHandler(BaseHTTPRequestHandler):
         except:
             page = "<b>"+"No page here! 404"+"</b>"
             self.send_response(404)
-        self.send_header('content-type','text/html')
+        self.send_header('content-type', 'text/html')
         self.end_headers()
-        
-        self.wfile.write(bytes(page,'utf-8'))
+
+        self.wfile.write(bytes(page, 'utf-8'))
 
     def do_POST(self):
-        self.send_header('content-type','text/html')
+        self.send_header('content-type', 'text/html')
         self.end_headers()
 
+
 def main():
-    address = ('localhost',8080)
-    server = HTTPServer(address,requestHandler)
+    address = ('localhost', 8080)
+    server = HTTPServer(address, requestHandler)
     server.serve_forever()
 
 
