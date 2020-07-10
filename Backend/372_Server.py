fc3e9f21c7043be0a7c166eea4ebf89950b95dc5
AlfaPigeon <oguzkaan.ozsoy@hotmail.com>
2020-07-10 20:59:52 +0300

    Backend files created

---
+++ Backend/372_Server.py
@@ -0,0 +1,30 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class requestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            page = open("../Frontend"+self.path).read()
+            self.send_response(200)
+        except:
+            page = "<b>"+"No page here! 404"+"</b>"
+            self.send_response(404)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+        
+        self.wfile.write(bytes(page,'utf-8'))
+
+    def do_POST(self):
+        self.send_header('content-type','text/html')
+        self.end_headers()
+
+def main():
+    address = ('localhost',8080)
+    server = HTTPServer(address,requestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
