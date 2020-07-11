66f71f75227fb68b9863dab4840fa80be37d9864
AlfaPigeon <oguzkaan.ozsoy@hotmail.com>
2020-07-11 14:22:41 +0300

    Backend and frontend changes

--- Backend/372_Server.py
+++ Backend/372_Server.py
@@ -1,7 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-WhiteList_GET = ['/', '/index.html']
-WhiteList_POST = []
+WhiteList_GET = ['/', '/index', '/login',
+                 '/css/index.css', '/css/test.css', '/css/login.css']
+WhiteList_POST = ['/api/login']
 
 
 class requestHandler(BaseHTTPRequestHandler):
@@ -13,16 +14,7 @@ class requestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             return
 
-        if self.path == '/':
-            self.path = '/index.html'
-        try:
-            page = open("../Frontend"+self.path).read()
-            self.send_response(200)
-        except:
-            page = "<b>"+"No page here! 404"+"</b>"
-            self.send_response(404)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
+        page = self.GET_API()
 
         self.wfile.write(bytes(page, 'utf-8'))
 
@@ -32,8 +24,79 @@ class requestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
             return
+        self.POST_API()
+
+    def GET_API(self):
+        if(self.path.split("/")[1] == "css"):
+            return self.css_GET()
+        elif(self.path.split("/")[1] == "js"):
+            return self.js_GET()
+        elif(self.path == "/" or self.path == "/index"):
+            return self.index_GET()
+        elif(self.path == "/login"):
+            return self.login_GET()
+
+    def POST_API(self):
+        if(self.path == '/api/login'):
+            return self.login_form_POST()
+
+    def index_GET(self):
+        try:
+            page = open("../Frontend/index.html").read()
+            self.send_response(200)
+        except:
+            page = "<b>"+"No page here! 404"+"</b>"
+            self.send_response(404)
         self.send_header('content-type', 'text/html')
         self.end_headers()
+        return page
+
+    def login_GET(self):
+        try:
+            page = open("../Frontend/login.html").read()
+            self.send_response(200)
+        except:
+            page = "<b>"+"No page here! 404"+"</b>"
+            self.send_response(404)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        return page
+
+    def css_GET(self):
+        try:
+            page = open("../Frontend"+self.path).read()
+            self.send_response(200)
+        except:
+            page = "<b>"+"No css here! 404"+"</b>"
+            self.send_response(404)
+        self.send_header('content-type', 'text/css')
+        self.end_headers()
+        return page
+
+    def js_GET(self):
+        try:
+            page = open("../Frontend"+self.path).read()
+            self.send_response(200)
+        except:
+            page = "<b>"+"No css here! 404"+"</b>"
+            self.send_response(404)
+        self.end_headers()
+        return page
+
+    def login_form_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length).decode('utf-8')
+        print(body)
+
+        self.send_response(301)
+        if(body.split("&")[1].split("=")[1] == "123"):
+            self.send_header('Location','/')
+        else:
+            self.send_header('Location','/login')
+
+        self.end_headers()
+        self.wfile.write(bytes("ok", 'utf-8'))
+        return True
 
 
 def main():
